a6705efe01d0aac3fb1dd501b2ce9dababb623a9
shohei niki <s.niki.326@gmail.com>
2017-06-19 17:15:41 +0900

    latest commit

--- app.rb
+++ app.rb
@@ -11,7 +11,8 @@ end
 
 post '/callback' do
   body = request.body.read
-  put "へろく〜〜〜〜"
+  p "へろく〜〜〜〜"
+  p body
   signature = request.env['HTTP_X_LINE_SIGNATURE']
   unless client.validate_signature(body, signature)
     error 400 do 'Bad Request' end
